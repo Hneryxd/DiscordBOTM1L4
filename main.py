22851678100c33322207ff1a4afb5f87597b72a6
Hneryxd <davidllerena1124@gmail.com>
2025-07-19 14:19:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
+import random
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='?', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -36,8 +37,17 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
-@cool.command(name='bot')
+@bot.command(name='bot')
 async def _bot(ctx):
-    await ctx.send('Yes, the bot is cool.')
+    await ctx.send('Si, soy muy cool.  :sunglasses:')
+ 
+@bot.command()
+async def coinflip(ctx):
+        x = random.randint(0, 1)
+        if x == 0:
+            resultado = 'sello'
+        else:
+            resultado = 'cara'
+        await ctx.send(f'He lanzado tu moneda, y cayó en...  ' + resultado )
 
 bot.run("token")
