29663ceb047519f57b11c74fef0a652f0670eca4
Hneryxd <davidllerena1124@gmail.com>
2025-07-19 13:54:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -29,18 +29,15 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
     await ctx.send(left + right)
     
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
 
 @cool.command(name='bot')
 async def _bot(ctx):
-    """Is the bot cool?"""
     await ctx.send('Yes, the bot is cool.')
 
 bot.run("token")
